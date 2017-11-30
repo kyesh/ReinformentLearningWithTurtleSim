407243d10de4790bb24672eacca7506c9cdd5a11
Ubuntu_16_4 <kyesh@umich.edu>
2017-11-29 22:27:39 -0500

    Framework done

--- catkin_ws/src/reinforcement_planner/src/main.cpp
+++ catkin_ws/src/reinforcement_planner/src/main.cpp
@@ -1,10 +1,51 @@
+#include <math.h>
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "turtlesim/Pose.h"
 
 ros::Publisher action;
+double XG = 1;
+double YG = 1;
+
+//Forces range between - pi and pi
+double fixThetaRange(double theta){
+
+	theta = fmod(theta,2*M_PI);//Brings between -2pi and pi
+	if(theta > M_PI){//convert to negative pi if greater than pi
+		theta =  theta - 2*M_PI;
+	}else if(theta < -M_PI){
+		theta = 2*M_PI - theta;
+	}
+
+	return theta;
+
+}
+
+//Gets anglular difference from a to be between -pi/2 and pi/2
+double angleDiff(double a , double b){
+	return fixThetaRange(b-a);
+}
+
+geometry_msgs::Twist policy(double xt, double yt, double xg, double yg, double thetat){
+
+	double thetag = atan2(yg - yt, xg - xt);
+	double thetad = angleDiff(thetat, thetag);
+	double a = yg - yt;
+	double b =  xg - xt;
+	double d = sqrt(a*a + b*b);
+
+	geometry_msgs::Twist msg;
+
+	msg.linear.x = 1;
+	msg.angular.z = 1;
+
+	return msg;
+}
 
 void turtleSimPos_callback(const turtlesim::Pose::ConstPtr& pos){
+
+	geometry_msgs::Twist msg = policy(pos->x, pos->y, XG, YG, pos->theta);
+	action.publish(msg);
 
 }
 
